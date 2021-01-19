c341be2c1da4faeafefffdaf284d1bc9178732db
FlanGrande <francisjgrande@gmail.com>
2021-01-19 02:05:55 +0000

    First steps in a good direction
    Added most 3D necessary changes like replacing Node2D with Spatial and that kind of thing. Also tried to adjust the code as required.

--- Boid.gd
+++ Boid.gd
@@ -1,27 +1,40 @@
-extends Node2D
+extends Spatial
 
-class_name Boid
+class_name Boid3D
 
 # Boid's x, y origin
 var x = 0
 var y = 0
+var z = 0
 # Boid's direction vector
 var dx = 0
 var dy = 0
+var dz = 0
 var d = 20
 var offset = 10
-# Boid history of positions?
+# Boid history of positions
 var history = []
 
 
 func _ready():
 	pass
 
-func initBoid(window_width, window_height):
-	x = rand_range(0, window_width)
-	y = rand_range(0, window_height)
+func initBoid(window_width, window_height, movement_depth):
+	x = rand_range(-window_width, window_width)
+	y = rand_range(-window_height, window_height)
+	z = rand_range(-movement_depth, movement_depth)
 	dx = rand_range(0, d) - offset
 	dy = rand_range(0, d) - offset
+	dz = rand_range(0, d) - offset
 	history = []
 	return self
 
+func _process(delta):
+	var translation_vector = Vector3(0, 0, 3.0)
+	$ImmediateGeometry.translation = translation_vector
+	#print(Vector3(x, y, z))
+	$ImmediateGeometry.clear()
+	$ImmediateGeometry.begin(Mesh.PRIMITIVE_TRIANGLES)
+	$ImmediateGeometry.set_color(Color.red)
+	$ImmediateGeometry.add_sphere(8.0, 8.0, 0.5)
+	$ImmediateGeometry.end()
--- BoidNode.gd
+++ BoidNode.gd
@@ -1,14 +1,15 @@
-extends Node2D
+extends Spatial
 
 # Based on: https://github.com/beneater/boids/blob/master/boids.js
 
-onready var boid_scene = preload("res://Boid.tscn")
+onready var boid_scene = preload("res://Boid3D.tscn")
 
-var window_width = OS.get_window_size().x
-var window_height = OS.get_window_size().y
+var window_width = 16.0
+var window_height = 16.0
+var forward_depth = 16.0
 
-const numBoids = 100
-const visualRange = 75
+const numBoids = 20
+const visualRange = 0.1
 
 var boids = []
 
@@ -16,7 +17,7 @@ var boids = []
 func _ready():
 	for i in range(numBoids):
 		var new_boid = boid_scene.instance()
-		boids.push_back(new_boid.initBoid(window_width, window_height))
+		boids.push_back(new_boid.initBoid(window_width, window_height, forward_depth))
 		add_child(new_boid)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,7 +33,8 @@ func _process(delta):
 		#Update the position based on the current velocity
 		boid.x += boid.dx;
 		boid.y += boid.dy;
-		boid.history.push_back([boid.x, boid.y])
+		boid.z += boid.dz;
+		boid.history.push_back([boid.x, boid.y, boid.z])
 		if(boid.history.size() > 50): # This will probably give me some issues.
 			boid.history.pop_front() # This will probably give me some issues.
 		
@@ -41,102 +43,125 @@ func _process(delta):
 
 # Constrain a boid to within the window. If it gets too close to an edge,
 # nudge it back in and reverse its direction.
-func keepWithinBounds(boid):
-	var margin = 50 # CONST?
-	var turnFactor = 1 # CONST?
+func keepWithinBounds(boid : Boid3D):
+	var margin = 30.0 # CONST?
+	var turnFactor = 1.0 # CONST?
 	
 	if (boid.x < margin):
 		boid.dx += turnFactor
-		
+	
 	if (boid.x > window_width - margin):
 		boid.dx -= turnFactor
-		
+	
 	if (boid.y < margin):
 		boid.dy += turnFactor
-		
+	
 	if (boid.y > window_height - margin):
 		boid.dy -= turnFactor
+	
+	if (boid.z < margin):
+		boid.dz += turnFactor
+	
+	if (boid.z > forward_depth - margin):
+		boid.dz -= turnFactor
 
 
 # Find the center of mass of the other boids and adjust velocity slightly to
 # point towards the center of mass.
-func flyTowardsCenter(boid : Boid):
-	var boidPosition = Vector2(boid.x, boid.y)
+func flyTowardsCenter(boid : Boid3D):
+	var boidPosition = Vector3(boid.x, boid.y, boid.z)
 	var centeringFactor = 0.005; # adjust velocity by this % # CONST?
 	
 	var centerX = 0;
 	var centerY = 0;
+	var centerZ = 0;
 	var numNeighbors = 0;
 	
 	for otherBoid in boids:
-		var otherBoidPosition = Vector2(otherBoid.x, otherBoid.y)
+		var otherBoidPosition = Vector3(otherBoid.x, otherBoid.y, otherBoid.z)
 		if(boidPosition.distance_to(otherBoidPosition) < visualRange):
 			centerX += otherBoid.x
 			centerY += otherBoid.y
+			centerZ += otherBoid.z
 			numNeighbors += 1
 	
 	if(numNeighbors >= 0):
 		centerX = centerX / numNeighbors
 		centerY = centerY / numNeighbors
+		centerZ = centerZ / numNeighbors
 		boid.dx += (centerX - boid.x) * centeringFactor
 		boid.dy += (centerY - boid.y) * centeringFactor
+		boid.dz += (centerZ - boid.z) * centeringFactor
 
 
 # Move away from other boids that are too close to avoid colliding
-func avoidOthers(boid : Boid):
-	var boidPosition = Vector2(boid.x, boid.y)
-	var minDistance = 20 # The distance to stay away from other boids # CONST?
+func avoidOthers(boid : Boid3D):
+	var boidPosition = Vector3(boid.x, boid.y, boid.z)
+	var minDistance = 4.0 # The distance to stay away from other boids # CONST?
 	var avoidFactor = 0.05 # Adjust velocity by this % # CONST?
 	var moveX = 0
 	var moveY = 0
+	var moveZ = 0
 	
 	for otherBoid in boids:
-		var otherBoidPosition = Vector2(otherBoid.x, otherBoid.y)
+		var otherBoidPosition = Vector3(otherBoid.x, otherBoid.y, otherBoid.z)
 		if(otherBoid != boid): # Is it really different as in javascript?
 			if(boidPosition.distance_to(otherBoidPosition) < minDistance):
 				moveX += boid.x - otherBoid.x;
 				moveY += boid.y - otherBoid.y;
+				moveZ += boid.z - otherBoid.z;
 	
 	boid.dx += moveX * avoidFactor
 	boid.dy += moveY * avoidFactor
+	boid.dz += moveZ * avoidFactor
 
 # Find the average velocity (speed and direction) of the other boids and
 # adjust velocity slightly to match.
-func matchVelocity(boid : Boid):
-	var boidPosition = Vector2(boid.x, boid.y)
+func matchVelocity(boid : Boid3D):
+	var boidPosition = Vector3(boid.x, boid.y, boid.z)
 	var matchingFactor = 0.05 # Adjust by this % of average velocity # CONST?
 	
 	var avgDX = 0
 	var avgDY = 0
+	var avgDZ = 0
 	var numNeighbors = 0
 	
 	for otherBoid in boids:
-		var otherBoidPosition = Vector2(otherBoid.x, otherBoid.y)
+		var otherBoidPosition = Vector3(otherBoid.x, otherBoid.y, otherBoid.z)
 		if(boidPosition.distance_to(otherBoidPosition) < visualRange):
 			avgDX += otherBoid.dx
 			avgDY += otherBoid.dy
+			avgDZ += otherBoid.dz
 			numNeighbors += 1
 	
 	if(numNeighbors >= 0):
 		avgDX = avgDX / numNeighbors
 		avgDY = avgDY / numNeighbors
+		avgDZ = avgDZ / numNeighbors
 		
 		boid.dx += (avgDX - boid.dx) * matchingFactor
 		boid.dy += (avgDY - boid.dy) * matchingFactor
+		boid.dz += (avgDZ - boid.dz) * matchingFactor
 
 # Speed will naturally vary in flocking behavior, but real animals can't go
 # arbitrarily fast.
-func limitSpeed(boid : Boid):
-	var speedLimit = 5 # CONST?
-	var speed = sqrt(boid.dx * boid.dx + boid.dy * boid.dy) # CONST?
+func limitSpeed(boid : Boid3D):
+	var speedLimit = 0.3# CONST?
+	var speed = pow(boid.dx * boid.dx + boid.dy * boid.dy + boid.dz * boid.dz, 1.0/3.0) # CONST?
 	
 	if(speed > speedLimit):
 		boid.dx = (boid.dx / speed) * speedLimit
 		boid.dy = (boid.dy / speed) * speedLimit
+		boid.dz = (boid.dz / speed) * speedLimit
 
-func drawBoid(boid : Boid):
-	boid.position = Vector2(boid.x, boid.y)
-	boid.rotation = atan2(boid.dy, boid.dx)
+func drawBoid(boid : Boid3D):
+	boid.translation = Vector3(boid.x, boid.y, boid.z)
+	boid.rotation = Vector3(atan2(boid.dx, boid.dy), atan2(boid.dy, boid.dx), abs(atan2(boid.dz, boid.dy))) # This will have to be fixed for sure.
+	#boid.rotation = Vector3(atan2(boid.dy, boid.dx), atan2(boid.dz, boid.dy), atan2(boid.dx, boid.dz)) # This will have to be fixed for sure.
+	#boid.translate(Vector3(boid.dx, boid.dy, boid.dz))
+	#boid.translate(Vector3(boid.x, boid.y, boid.z))
+	#boid.look_at(Vector3(boid.dx, boid.dy, boid.dz), Vector3.UP)
+	#boid.look_at_from_position(Vector3(boid.x, boid.y, boid.z), Vector3(boid.dx, boid.dy, boid.dz), Vector3.UP)
 	#move_to
 	#draw_trail
 	pass
